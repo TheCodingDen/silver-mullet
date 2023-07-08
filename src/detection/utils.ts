e417beedae4426972c29383b928b87654d63278f
nullishamy <git@amyerskine.me>
2023-07-08 19:09:50 +0100

    fix: centralise type

--- src/detection/utils.ts
+++ src/detection/utils.ts
@@ -1,6 +1,7 @@
 import { ActionRowBuilder, APIEmbed, ButtonBuilder, ButtonStyle, Guild, Message, TextBasedChannel } from 'discord.js'
 import _ from 'lodash'
 import { addQueuedAction, fetchQueuedActionByAuthorId } from '../cache/op'
+import { ActionUpgradeTo } from '../clients/redis'
 import color from '../utils/color'
 import { embedBase, messageLink } from '../utils/discordUtils'
 import { ActionFunction } from './actions'
@@ -32,7 +33,7 @@ export async function getQueueChannel (guild: Guild): Promise<TextBasedChannel> 
 
 interface MakeComponentOpts {
   disabled: boolean
-  confirmAction: 'kick' | 'ban'
+  confirmAction: ActionUpgradeTo
 }
 export function makeComponents ({ disabled, confirmAction }: MakeComponentOpts): ActionRowBuilder<ButtonBuilder> {
   const confirm = new ButtonBuilder()
@@ -55,7 +56,7 @@ export function makeComponents ({ disabled, confirmAction }: MakeComponentOpts):
 // Allow new posts to be queued to the action queue by the same user after one hour
 const newActionPostThresholdMillis = 1000 * 60 * 60
 
-export function makeQueueCallback (action: 'kick' | 'ban'): ActionFunction {
+export function makeQueueCallback (action: ActionUpgradeTo): ActionFunction {
   return async (member, message, result) => {
     const queuedAction = await fetchQueuedActionByAuthorId(message.author.id)
     if (queuedAction) {
