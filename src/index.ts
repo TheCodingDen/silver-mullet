eaee0f70e9d1539f0f9853acbf0803b351e14d7f
Amy <git@amyerskine.me>
2023-07-17 01:21:45 +0100

    fix: lint

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ void (async () => {
   logger.info('Startup process complete.')
 
   // Catch any unhandled rejections (mainly from d.js) so that they dont crash us & we get nice logs
-  process.on("unhandledRejection", (err) => {
-    logger.error(`Encountered unexpected exception:\n${errStack(err)}`);
-  });
+  process.on('unhandledRejection', (err) => {
+    logger.error(`Encountered unexpected exception:\n${errStack(err)}`)
+  })
 })().catch(err => logger.error(`Startup failed: ${errStack(err)}`))
