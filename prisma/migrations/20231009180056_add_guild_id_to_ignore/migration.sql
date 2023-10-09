f4d83891463d38b783b27ed20b6916abce4371be
Linus Willner <hello@linuswillner.me>
2023-10-10 01:42:56 +0300

    Make migration self-heal the data

--- prisma/migrations/20231009180056_add_guild_id_to_ignore/migration.sql
+++ prisma/migrations/20231009180056_add_guild_id_to_ignore/migration.sql
@@ -5,5 +5,30 @@
 
 */
 
+CREATE TABLE ignore_temp (
+    id text,
+    snowflake text,
+    type "IgnoreTarget",
+    "guildId" text
+);
+
+INSERT INTO ignore_temp (id, snowflake, type, "guildId")
+SELECT
+  id,
+  snowflake,
+  type,
+  CASE WHEN snowflake = '731582778809909351' -- Only one ignore in the test guild
+    THEN '731581715474153542' -- Test
+    ELSE '172018499005317120' -- Main
+  END AS "guildId"
+FROM "Ignore";
+
+TRUNCATE TABLE "Ignore";
+
 -- AlterTable
 ALTER TABLE "Ignore" ADD COLUMN     "guildId" TEXT NOT NULL;
+
+INSERT INTO "Ignore" (id, snowflake, type, "guildId")
+SELECT * FROM ignore_temp;
+
+DROP TABLE ignore_temp;
