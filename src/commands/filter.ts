323d0ce3fc2e506ca4d6a15cfe8ad051e42ef8b3
Linus Willner <hello@linuswillner.me>
2023-10-10 01:55:04 +0300

    Hotfix: Truncate overly long parameter description

--- src/commands/filter.ts
+++ src/commands/filter.ts
@@ -45,7 +45,7 @@ export default class FilterCommand extends SlashCommand {
             {
               type: CommandOptionType.STRING,
               name: 'flags',
-              description: 'The flags to use. defaults to "g". Pass "none" to have no flags. Strings are automatically lowercased, so "i" has no effect.',
+              description: 'The flags to use. Defaults to "g", "i" is always enabled. Pass "none" to have no flags.',
               required: false
             }
           ]
