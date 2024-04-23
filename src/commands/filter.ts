61d34cae7e9d6ca1f1b3e842f18b36dc21e8eacd
nullishamy <git@amyerskine.me>
2024-04-23 21:22:51 +0100

    fix: allow empty regexes

--- src/commands/filter.ts
+++ src/commands/filter.ts
@@ -41,7 +41,7 @@ export default class FilterCommand extends SlashCommand {
             {
               type: CommandOptionType.STRING,
               name: 'regex',
-              description: 'The regex to trigger with. Supplied as-is, without / / syntax.',
+              description: 'The regex to trigger with. Supplied as-is, without / / syntax. "none" to match nothing.',
               required: true
             },
             {
@@ -311,6 +311,10 @@ export default class FilterCommand extends SlashCommand {
 
     if (flags === 'none') {
       flags = ''
+    }
+
+    if (rawRegex === 'none') {
+      rawRegex = ''
     }
 
     let regex: RegExp
