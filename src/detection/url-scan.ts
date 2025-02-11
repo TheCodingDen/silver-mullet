34fa81df8d504a27fefb5e6dd199d1fbd3b90cc7
nullishamy <git@amyerskine.me>
2025-02-11 21:12:53 +0000

    style: lints

--- src/detection/url-scan.ts
+++ src/detection/url-scan.ts
@@ -70,8 +70,7 @@ async function pollForResult (submission: ScanCreateResponse, accountId: string,
   try {
     const res = await cloudflare.urlScanner.scans.get(submission.uuid, params)
     // @ts-expect-error CF SDK is missing the `history` property which has our redirect chain
-    const redirects: string[] = res.page.history?.map(h => h.url) ?? []
-    
+    const redirects: string[] = res.page.history?.map(h => h.url) ?? []    
     return {
       domain: res.task.domain,
       url: redirects[0] ?? res.task.url,
