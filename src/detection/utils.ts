70536ea38c84a1c71c75cc347c17c752a9bbf2f4
Cassandra Choi <nrocinu.lahwran@gmail.com>
2025-12-01 18:19:13 -0500

    fix: removing failing postinstall script; remove link scanning (and therefore patch-package) (#73)

--- src/detection/utils.ts
+++ src/detection/utils.ts
@@ -8,8 +8,8 @@ import { channelLink, embedBase } from '../utils/discordUtils'
 import { RetryResult } from '../utils/retry'
 import { ActionFunction, TriggeringMessage } from '../actions'
 import { Comparison } from './spam-detection'
-import { DetectionResult, URLDetectionResult, DetectionSource, FilterDetectionResult, SpamDetectionResult, ReactivationDetectionResult } from './types'
-import { BadLink } from './url-scan'
+import { DetectionResult, DetectionSource, FilterDetectionResult, SpamDetectionResult, ReactivationDetectionResult } from './types'
+import { checkExhaustive } from '../utils/type-checks'
 
 async function getChannel (guild: Guild, name: string, id: string | undefined): Promise<TextBasedChannel> {
   if (!id) {
@@ -206,37 +206,6 @@ ${message.content.trimStart().trimEnd() || '<no-content>'}
   }
 }
 
-function makeURLDefaultEmbed (message: TriggeringMessage, result: URLDetectionResult): APIEmbed {
-  const showURL = (url: BadLink): string => {
-    const categories = url.categories.map(c => `**${c}**`).join(', ')
-    const urls = url.redirects.map(u => `- ${u}`).join('\n') || '[No redirects known]'
-    const reportURL = `[View full report on Cloudflare Radar](${url.reportURL})`
-
-    return `Final destination: ${url.url}\nCategories: ${categories}\n\nRedirect chain:\n${urls}\n\n${reportURL}`
-  }
-
-  return {
-    ...embedBase(),
-    title: 'Malicious URL detected',
-    color: color.red,
-    author: {
-      name: `@${message.author.user.username} (${message.author.id})`,
-      icon_url: message.author.displayAvatarURL()
-    },
-    description: `
-          **Triggered in** (${channelLink(message.channel.id)}):
-          \`\`\`
-${message.content.trimStart().trimEnd() || '<no-content>'}
-          \`\`\` 
-          **Action taken**:
-          \`${result.action}\`
-
-          **Scanned URLs**:
-${result.trippedURLs.map(showURL).join('\n\n').trimStart().trimEnd()}
-        `
-  }
-}
-
 function makeFilterDefaultEmbed (message: TriggeringMessage, result: FilterDetectionResult): APIEmbed {
   const { action, trippedFilter } = result
   const previousContent = result.oldMessage !== undefined
@@ -279,7 +248,7 @@ export function makeDefaultEmbed (message: TriggeringMessage, result: DetectionR
   } else if (result.source === DetectionSource.REACTIVATION) {
     return makeReactivationDefaultEmbed(message, result)
   } else {
-    return makeURLDefaultEmbed(message, result)
+    checkExhaustive(result)
   }
 }
 
